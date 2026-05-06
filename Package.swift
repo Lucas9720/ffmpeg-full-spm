96d11d0b88e7136d95b41285bc6780d7c45d2853
Lucas9720 <159280934+Lucas9720@users.noreply.github.com>
2026-05-06 13:33:23 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FFmpegBinary",
-            url: "https://github.com/Lucas9720/ffmpeg-full-spm/releases/download/1.0.4/FFmpeg.xcframework.zip",
+            url: "https://github.com/Lucas9720/ffmpeg-full-spm/releases/download/1.0.6/FFmpeg.xcframework.zip",
             checksum: "4c187e4292b06bf9ef08f8a5768f34426c556c979392f18f9294e7c806e94214"
         )
     ]
