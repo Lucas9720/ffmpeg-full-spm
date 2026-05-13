ce4d67fb6bd7a08b7aa2e79938ab20696fdc8ded
Lucas9720 <159280934+Lucas9720@users.noreply.github.com>
2026-05-13 17:53:47 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,13 +5,19 @@ let package = Package(
     name: "FFmpeg",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "FFmpeg", targets: ["FFmpeg"])
+        .library(name: "FFmpeg", targets: ["FFmpegWrapper"])
     ],
     targets: [
+        .target(
+            name: "FFmpegWrapper",
+            dependencies: ["FFmpegBinary"],
+            path: "Sources/FFmpegWrapper",
+            sources: ["shim.swift"]
+        ),
         .binaryTarget(
-            name: "FFmpeg",
-            url: "https://github.com/Lucas9720/ffmpeg-full-spm/releases/download/1.1.1/FFmpeg.xcframework.zip",
-            checksum: "9bf54c1c1d6ea6121982bd558de120057d6f87d8edb3abad81f80d6f92995be4"
+            name: "FFmpegBinary",
+            url: "https://github.com/Lucas9720/ffmpeg-full-spm/releases/download/1.1.2/FFmpeg_Nuevo.xcframework.zip",
+            checksum: "CALCULA_EL_CHECKSUM_DE_FFmpeg_Nuevo.xcframework.zip"
         )
     ]
 )
